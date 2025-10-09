3a949ea92c024d73fcfd92697dcc48fbfa7bb480
vominhcuong2501 <minhcuongvo2501@gmail.com>
2025-10-09 20:03:32 +0700

    Section the classic Fizzbuzz + Aray Chunking

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,22 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        switch (true) {
+            case (i % 3 === 0 && i % 5 === 0):
+                console.log('fizzbuzz')
+                break;
+            case (i % 3 === 0):
+                console.log('fizz')
+                break;
+            case (i % 5 === 0):
+                console.log('buzz')
+                break;
+            default:
+                console.log(i)
+        }
+    }
+}
 
 module.exports = fizzBuzz;
