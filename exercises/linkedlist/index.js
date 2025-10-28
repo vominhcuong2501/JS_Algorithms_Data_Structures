f7226b1b47031bd088d4fac4d7769a1d530ec320
vominhcuong2501 <minhcuongvo2501@gmail.com>
2025-10-28 20:39:54 +0700

    Update linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -27,13 +27,51 @@ class LinkedList {
         }
         return count;
     }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+    }
+
+    clear() {
+        this.head = null
+    }
+
+    removeFirst() {
+        if (!this.head) return;
+
+        this.head = this.head.next;
+    }
+
+    removeLast() {
+        if (!this.head) return
+
+        if (!this.head.next) {
+            this.head = null
+            return
+        }
+
+        let previous = this.head;
+        let node = this.head.next;
+        while (node.next) {
+            previous = node;
+            node = node.next;
+        }
+        previous.next = null;
+    }
 }
 
-const list = new LinkedList();
-
-list.insertFirst(1);
-list.insertFirst(1);
-list.insertFirst(1);
-list.insertFirst(1);
 
 module.exports = { Node, LinkedList };
