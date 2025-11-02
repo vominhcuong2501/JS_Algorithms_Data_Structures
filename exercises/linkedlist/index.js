ca4e6d177c6f3642327b807706d007a0f54bc6a4
vominhcuong2501 <minhcuongvo2501@gmail.com>
2025-11-02 09:18:24 +0700

    Apply linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -133,11 +133,20 @@ class LinkedList {
         previous.next = node;
     }
 
-    forEach(callback) {
+    forEach(fn) {
         let node = this.head;
-
+        let counter = 0
         while (node) {
-            callback(node);
+            fn(node, counter);
+            node = node.next
+            counter++;
+        }
+    }
+
+    *[Symbol.iterator]() {
+        let node = this.head;
+        while (node) {
+            yield node;
             node = node.next;
         }
     }
