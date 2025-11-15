d646363d6242dcfcdcf96ba167eac4c7b4c1a8e3
vominhcuong2501 <minhcuongvo2501@gmail.com>
2025-11-15 21:32:09 +0700

    update code

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,10 +3,36 @@
 
 function bubbleSort(arr) {
 
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    return arr;
 }
 
 function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
 
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+
+        if (indexOfMin !== i) {
+            let temp = arr[i];
+            arr[i] = arr[indexOfMin];
+            arr[indexOfMin] = temp;
+        }
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
